252c60d3fe1582d8b7e43e9b92a4bba880f6479e
Beatriz Cristina <biacristinafs@hotmail.com>
2021-03-02 01:02:15 -0300

    Iniciando conexão HTTPS

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,19 @@
 const express = require('express');
+const fs = require('fs');
+const https = require('https');
+const path = require('path');
 const routes = require('./routes');
 require('./database/index');
 
+const httpsOptions = {
+    cert: fs.readFileSync(path.join(__dirname, 'config','ssl', 'server.crt')),
+    key: fs.readFileSync(path.join(__dirname, 'config','ssl', 'server.key'))
+};
 const app = express();
     app.use(express.json());
     app.use(routes);
-    app.listen(3000);
+    //app.listen(3000);
+
+https.createServer(httpsOptions, app).listen(3000, function(){
+    console.log('Servidor funcionando em https://localhost:3000');
+});
